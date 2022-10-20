f61473ab75e02329c9cf8a6ac1b063c50710e4cf
kids_seek_options <99182431+Lycher2@users.noreply.github.com>
2022-10-20 21:55:25 +0300

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -10,31 +10,30 @@ from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objects as go
 
-st.title("Facebook Prophet - Stock Price Prediction")
+st.title("Facebook Prophet - Crypto Price Prediction")
 st.write("This is a demonstrator app created on Streamlit to show a use case of the Facebook Prophet "
-         "timeseries model to forecast stock market data.")
+         "timeseries model to forecast crypto market data.")
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 @st.experimental_memo
-def load_stock_data(ticker:str):
+def load_crypto_data(ticker:str):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace=True)
     return data
 
-tickers = ["VOO", "AAPL", "GOOG", "TSLA", "MSFT", "GME"]
-
+tickers = ["BTC-USD","ETH-USD"]
 ticker = st.selectbox("Select tickers", tickers)
 
 if "years_slider"not in st.session_state:
     st.session_state.years_slider = 1
 
-n_years = st.slider("Years of prediction:", 1, 5, key="years_slider")
-period = n_years * 365
+n_month = st.slider("months of prediction:", 1, 60, key="months_slider")
+period = n_month * 30
 
 data_load_state = st.text("Loading... {} data".format(ticker))
-data = load_stock_data(ticker)
+data = load_crypto_data(ticker)
 data_load_state.text("Loading... Complete!")
 st.write(data.tail())
 
@@ -51,32 +50,33 @@ def plot_data():
 plot_data()
 
 def make_prediction():
-    df_train = data[["Date", "Close"]]
-    df_train = df_train.rename(columns={"Date":"ds", "Close":"y"})
+    df_train = data[["Date", "Adj Close"]]
+    df_train['Date'] = df_train['Date'].dt.tz_localize(None)
+    df_train = df_train.rename(columns={"Date":"ds", "Adj Close":"y"})
     prediction_progress = st.progress(0)
     prediction_progress.progress(10)
 
-    model = Prophet()
-    model.fit(df_train)
+    pred = Prophet()
+    pred.fit(df_train)
 
     prediction_progress.progress(20)
 
-    future = model.make_future_dataframe(periods=period)
+    future = pred.make_future_dataframe(periods=365)
 
     prediction_progress.progress(40)
 
-    forecast = model.predict(future)
+    forecast_val = pred.predict(future)
 
     prediction_progress.progress(80)
 
     st.subheader("Forecast")
 
     st.write("Forecast timeseries")
-    fig1 = plot_plotly(model, forecast)
+    fig1 = plot_plotly(pred, forecast_val)
     st.plotly_chart(fig1)
 
     st.write("Forecast components")
-    fig2 = model.plot_components(forecast)
+    fig2 = pred.plot_components(forecast_val)
     st.write(fig2)
 
     prediction_progress.progress(100)
@@ -84,6 +84,5 @@ def make_prediction():
     st.success('Successfully forecasted {} stock close price'.format(ticker))
 
 make_prediction()
-
 if __name__ == '__main__':
     print("Run `streamlit run main.py` to run this app")
